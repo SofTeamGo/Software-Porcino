2471930706848a0fc469ab257b965b11c68d38db
Fernando Cruz Banegas <nano.fer777@gmail.com>
2018-07-21 12:36:36 -0400

    borre form

--- resources/views/reproductores/index.blade.php
+++ resources/views/reproductores/index.blade.php
@@ -6,45 +6,7 @@
 		<p class="mb-4">Bootstrap provides several form control styles, layout options, and custom components for creating a wide variety of forms.</p>
 		<div class="cardbox">
 			<div class="cardbox-body">
-			    <form method="get" action="/">
-			        <fieldset>
-			            <div class="form-group row">
-				            <label class="col-lg-2 col-form-label">With help</label>
-				            <div class="col-lg-10">
-				                <input class="form-control" type="text"><span class="form-text text-muted">A block of help text that breaks onto a new line and may extend beyond one line.</span>
-				            </div>
-			            </div>
-			        </fieldset>
-				    <fieldset>
-				        <div class="form-group row">
-				            <label class="col-lg-2 col-form-label" for="input-id-1">Label focus</label>
-				            <div class="col-lg-10">
-				                <input class="form-control" id="input-id-1" type="text">
-				            </div>
-				        </div>
-				    </fieldset>
-			        <fieldset>
-			            <div class="form-group row">
-			                <label class="col-lg-2 col-form-label">Password</label>
-			                <div class="col-lg-10">
-			                    <input class="form-control" type="password" name="password">
-			                </div>
-			            </div>
-			        </fieldset>
-			        <fieldset>
-			            <div class="form-group row">
-			                <label class="col-lg-2 col-form-label">Placeholder</label>
-			                <div class="col-lg-10">
-			                    <input class="form-control" type="text" placeholder="placeholder">
-			                </div>
-			            </div>
-			        </fieldset>
-			        <fieldset>
-			           <div class="float-right">
-                           <button class="btn btn-info" type="submit">Register</button>
-                       </div>
-                    </fieldset>
-			    </form>
+
 			    <example-form></example-form>
 			</div>
 		</div>
